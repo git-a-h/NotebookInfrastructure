9d4f9e1e26bff63d73a14f9cdc0ba837e766cbf1
git-a-h <146676192+git-a-h@users.noreply.github.com>
2025-10-30 12:14:32 +0700

    introduce coverage, makefile and update env.yaml

--- tests/test_dummy_add.py
+++ tests/test_dummy_add.py
@@ -1,12 +1,12 @@
-from src.step_1 import dummy_add
+from src.step_1.dummy_add import add
 import pytest
 
 def test_add_integers():
-    assert dummy_add.add(2, 3) == 5
-    assert dummy_add.add(-1, 1) == 0
+    assert add(2, 3) == 5
+    assert add(-1, 1) == 0
 
 def test_add_floats():
-    assert dummy_add.add(2.5, 2.5) - 5.0 < 1e-9
+    assert add(2.5, 2.5) - 5.0 < 1e-9
 
 def test_add_complex():
-    assert dummy_add.add(1+2j, 3+4j) == 4+6j
+    assert add(1+2j, 3+4j) == 4+6j
