1f975decda93f4672c58b3699ca6b7811c72e67c
git-a-h <146676192+git-a-h@users.noreply.github.com>
2025-12-02 14:34:26 +0100

    replace matpl with plt and lint check

--- tests/step_4/smoke_test.py
+++ tests/step_4/smoke_test.py
@@ -1,4 +1,9 @@
 def test_of_notebook():
-    import matplotlib as matpl
-    matpl.use("Agg")  # So the test does not open a window for the plt.show(), which has to be closed -> pytest hangs
-    import src.step_3.notebook # noqa: E402
+    import matplotlib as plt
+
+    plt.use(
+        "Agg"
+    )  # So the test does not open a window for the plt.show(), which has to be closed -> pytest hangs
+    import src.step_3.notebook  # noqa: F401
+
+    # F401 is imported but unused, but is run during smoketest (see htmlcoverage)
