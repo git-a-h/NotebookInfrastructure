fcb5ab634f2cf62f083cdf68a2fb17d3d4c6602c
git-a-h <146676192+git-a-h@users.noreply.github.com>
2025-12-13 17:32:45 +0100

    add automatic notebook conversion and upload

--- scripts/convert_notebooks.py
+++ scripts/convert_notebooks.py
@@ -1,17 +1,47 @@
 import pathlib
-import os 
+import os
 import sys
 import subprocess
 
 
-if __name__ == '__main__':
-    print('This script should collect all .py files that should be converted to notebooks')
-    print('Please provide the relative directory path from the project root as an command line argument, default will be src.')
-
+def find_notebooks(target_dir: str) -> list[str]:
     root = pathlib.Path(__file__).parent.parent.resolve()
-    target_dir = os.path.join(root, sys.argv[1]) if len(sys.argv) > 1 else os.path.join(root, 'src')
-    print(f'Target directory: {target_dir}')
+    target_dir = os.path.join(root, target_dir)
+    print(f"Target directory: {target_dir}")
 
-    notebooks_to_convert = [os.path.join(root,file) for root, _, files in os.walk(target_dir) for file in files if file.startswith('notebook') and file.endswith('.py')]
-    for notebook in notebooks_to_convert:
-        subprocess.run(['jupytext', '--to', 'ipynb', '--execute', notebook])
+    notebooks_to_convert = [
+        os.path.join(root, file)
+        for root, _, files in os.walk(target_dir)
+        for file in files
+        if file.startswith("notebook") and file.endswith(".py")
+    ]
+    return notebooks_to_convert
+
+
+def create_notebooks(target_dir: str) -> list[str]:
+    notebooks_to_convert = find_notebooks(target_dir)
+    notebooks_converted = []
+    for file in notebooks_to_convert:
+        subprocess.run(["jupytext", "--to", "ipynb", "--execute", file])
+        ipynb_file = pathlib.Path(file).with_suffix(".ipynb")
+        notebooks_converted.append(ipynb_file)
+    return notebooks_converted
+
+
+"""def write_to_file(notebooks_converted: list[str])-> None:
+    Write the converted notebooks to a file for later use in CI/CD pipeline, with line break as delimiter
+    current_dir = pathlib.Path(__file__).parent.resolve()
+    with open (os.path.join(current_dir, 'output', 'notebooks_converted.txt'),'w') as f:
+        for notebook in notebooks_converted:
+            f.write(f'{notebook}\n')"""
+
+if __name__ == "__main__":
+    print(
+        "This script should collect all .py files that should be converted to notebooks"
+    )
+    print(
+        "Please provide the relative directory path from the project root as an command line argument, default will be src."
+    )
+    target_dir = sys.argv[1] if len(sys.argv) > 1 else "src"
+    notebooks_converted = create_notebooks(target_dir)
+    # write_to_file(notebooks_converted)
---
+++ src/step_3/notebook_test.py
@@ -0,0 +1,8 @@
+# %% [markdown]
+## Welcome to this notebook! <br>
+# In this step, this notebook will guide you how to use the add function from step_1 <br>
+# First, we import all of the necessary packages. <br>
+# This is also nice to see that $\lambda$ works in markdown cell.
+
+# %%
+print("This is a test notebook for step 3")
