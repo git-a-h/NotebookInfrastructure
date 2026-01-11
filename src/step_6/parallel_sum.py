56e2704be892abc1a4ad5a0713367e24aa0f511f
git-a-h <146676192+git-a-h@users.noreply.github.com>
2026-01-11 22:00:36 +0100

    add parallel module and parallel tests

---
+++ src/step_6/parallel_sum.py
@@ -0,0 +1,13 @@
+from mpi4py import MPI
+
+T = int | float | complex
+def parallel_sum(xs : list [T]) -> T:
+    comm = MPI.COMM_WORLD
+    rank = comm.Get_rank()
+    size = comm.Get_size()
+
+    local_xs = xs[rank::size]
+    local_sum = sum(local_xs)
+
+    total_sum = comm.reduce(local_sum, op=MPI.SUM, root=0)
+    return total_sum
---
+++ tests/unit/parallel/test_parallel_sum.py
@@ -0,0 +1,8 @@
+from src.step_6.parallel_sum import parallel_sum
+from mpi4py import MPI
+
+def test_parallel_sum():
+    xs = list(range(5)) 
+    result = parallel_sum(xs)
+    if MPI.COMM_WORLD.Get_rank() == 0:
+        assert result == sum(xs)
