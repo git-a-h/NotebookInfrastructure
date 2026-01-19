8e406f526006bebefa5e71535fe6420b8fabe7a6
git-a-h <146676192+git-a-h@users.noreply.github.com>
2026-01-19 15:20:57 +0100

    fix script and add of failed assertion in notebook

--- scripts/convert-and-move-notebooks.sh
+++ scripts/convert-and-move-notebooks.sh
@@ -12,7 +12,7 @@ fi
 # Find all notebook*.py files, convert them to notebooks and move them to notebooks directory
 find "$project_root/src" -type f -name "notebook*.py" | while read file; do
     # convert notebooks
-    mpirun -np 2 jupytext --to ipynb --execute "$file"
+    mpirun --stdin none -np 2 jupytext --to ipynb --execute "$file"
     # move to notebooks directory
     mv "${file%.py}.ipynb" "$output_dir"  
 done
