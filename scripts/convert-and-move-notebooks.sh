98084f0a333b28bd4c0031c8ebfb5b4f91e102f7
git-a-h <146676192+git-a-h@users.noreply.github.com>
2026-01-19 16:30:53 +0100

    Step 6b (#7)

--- scripts/convert-and-move-notebooks.sh
+++ scripts/convert-and-move-notebooks.sh
@@ -12,7 +12,11 @@ fi
 # Find all notebook*.py files, convert them to notebooks and move them to notebooks directory
 find "$project_root/src" -type f -name "notebook*.py" | while read file; do
     # convert notebooks
-    jupytext --to ipynb --execute "$file"
+    mpirun --stdin none -np 2 jupytext --to ipynb --execute "$file"
+    if [ $? -ne 0 ]; then
+        echo "MPI process failed for $file"
+        exit 1
+    fi
     # move to notebooks directory
     mv "${file%.py}.ipynb" "$output_dir"  
 done
--- src/step_6/notebook_parallel_sum.py
+++ src/step_6/notebook_parallel_sum.py
@@ -20,3 +20,12 @@ from src.step_6.parallel_sum import parallel_sum  # noqa: E402
 
 xs = list(range(5))
 result = parallel_sum(xs)
+
+# %% [markdown]
+# Now, we add a cell, which tests the mpi parallism function and check if the size > 1
+
+# %%
+from mpi4py import MPI  # noqa: E402
+
+print(f"I am rank {MPI.COMM_WORLD.rank} of {MPI.COMM_WORLD.size}")
+assert MPI.COMM_WORLD.size > 1
